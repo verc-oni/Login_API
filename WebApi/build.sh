9f76730d7385ca48dc4bddff64fac375d4700e26
verc-oni <89543105+verc-oni@users.noreply.github.com>
2024-04-20 16:28:49 +0100

    uo

--- WebApi/build.sh
+++ WebApi/build.sh
@@ -4,10 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Todd_Arnold', 'Toddarnold@gmail.com', 'password')" | python manage.py shell
 python manage.py collectstatic --no-input
 
 python manage.py makemigrations
 
 # python manage.py migrate --fake api zero
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Todd_Arnold', 'Toddarnold@gmail.com', 'password')" | python manage.py shell
